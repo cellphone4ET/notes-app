f0c123927ab4c9ea35fddd70828578aa7068f0c0
cellphone4ET <ericacjohnson@gmail.com>
2019-04-22 20:10:17 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const getNotes = require('./utils.js');
+
+const notes = getNotes();
+
+
+
+// const add = require('./utils.js')
+// const sum = add(4, -2);
+// console.log(sum);
---
+++ utils.js
@@ -0,0 +1,10 @@
+const getNotes = function(x, y) {
+  return console.log("Your notes...");
+}
+
+module.exports = getNotes;
+
+// const add = function (x, y) {
+//   return x + y;
+// }
+// module.exports =  add;
