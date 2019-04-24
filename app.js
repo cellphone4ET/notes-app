6f2939f945620596ba79f31b12a065f1b014f585
cellphone4ET <ericacjohnson@gmail.com>
2019-04-23 21:35:02 -0400

    packages

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
+const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./utils.js');
 
-const notes = getNotes();
-
-
-
-// const add = require('./utils.js')
-// const sum = add(4, -2);
-// console.log(sum);
+console.log(yargs.argv)
